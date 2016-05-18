b0458a4859df06b559bfb2306653ec994bed8f5e
Nicolas Chambrier <naholyr@gmail.com>
2016-05-18 11:02:20 +0200

    refactor(): split implementations to allow later optimizations

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ if ( typeof WeakMap === 'undefined' || typeof Map === 'undefined' ) {
     // Browser globals (root is window)
     root.returnExports = factory();
   }
-}(this, function () {
+})(this, function () {
 
   var _idMap = new WeakMap();
   var _id = { id: 0 };
+
   // the last two arguments help with testing
   return function memoize(fn, cache, idMap, id) {
     if ( !cache ) {
@@ -35,41 +36,44 @@ if ( typeof WeakMap === 'undefined' || typeof Map === 'undefined' ) {
       id = _id;
     }
 
-    return function() {
-      var aKey = [];
-      for ( var i = 0; i < arguments.length; i++ ) {
-        var argType = typeof arguments[i];
-
-        // if the argument is not a primitive, get a unique (memoized?) id for it
-        if (
-          // typeof null is "object", although we'll consider it as a primitive
-          arguments[i] !== null &&
-          ( argType === 'object' || argType === 'function' || argType === 'symbol' )
-        ) {
-          if ( !idMap.has(arguments[i]) ) {
-            idMap.set(arguments[i], id.id++);
-          }
-          aKey.push( idMap.get(arguments[i]) );
-
-        // otherwise, add the argument and its type to the aKey
-        } else {
-          aKey.push( arguments[i] == null ?
-            '' + arguments[i] :
-            argType + '(' + arguments[i] + ')'
-          );
-        }
-      }
-
-      var sKey = aKey.join('_///_');
-
-      if ( !cache.has(sKey) ) {
-        var result = fn.apply(this, arguments);
-        cache.set( sKey, result );
-        return result;
-      }
-
-      return cache.get( sKey );
+    return function(/* ...args */) {
+      return memoizedCall(fn, cache, idMap, id, arguments);
     };
   };
 
-}));
+  function memoizedCall (fn, cache, idMap, id, args) {
+    var aKey = [];
+    for ( var i = 0; i < args.length; i++ ) {
+      var argType = typeof args[i];
+
+      // if the argument is not a primitive, get a unique (memoized?) id for it
+      if (
+        // typeof null is "object", although we'll consider it as a primitive
+        args[i] !== null &&
+        ( argType === 'object' || argType === 'function' || argType === 'symbol' )
+      ) {
+        if ( !idMap.has(args[i]) ) {
+          idMap.set(args[i], id.id++);
+        }
+        aKey.push( idMap.get(args[i]) );
+
+      // otherwise, add the argument and its type to the aKey
+      } else {
+        aKey.push( args[i] == null ?
+          '' + args[i] :
+          argType + '(' + args[i] + ')'
+        );
+      }
+    }
+
+    var sKey = aKey.join('_///_');
+
+    if ( !cache.has(sKey) ) {
+      var result = fn.apply(this, args);
+      cache.set( sKey, result );
+      return result;
+    }
+
+    return cache.get( sKey );
+  }
+})
